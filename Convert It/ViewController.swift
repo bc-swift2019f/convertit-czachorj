fd92674376c6ee9b6963750b9c96532fc9dda3aa
Jess <jessica@czachor.com>
2019-10-03 10:59:44 -0400

    6.5 Completed

--- Convert It/ViewController.swift
+++ Convert It/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var fromUnitsLabel: UILabel!
     @IBOutlet weak var resultsOutlet: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
+    @IBOutlet weak var decimalSegment: UISegmentedControl!
     
     var formulaArray = ["miles to kilometers", "kilometers to miles", "feet to meters", "yards to meters", "meters to feet", "meters to yards"]
     
@@ -25,12 +26,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
+        conversionStrings = formulaArray[formulaPicker.selectedRow(inComponent: 0)]
     }
     
     func calculateConversion() {
+        
+        guard let inputValue = Double(userInput.text!) else {
+            print("show alert here to say the value entered was not a number")
+            return
+        }
         var outputValue = 0.0
-        if let inputValue = Double(userInput.text!) {
-            
             switch conversionStrings {
             case "miles to kilometers":
                 outputValue = inputValue / 0.62137
@@ -47,13 +52,18 @@ class ViewController: UIViewController {
             default:
                 print("Show alert")
             }
-            resultsOutlet.text = "\(inputValue) \(fromUnits) = \(outputValue) = \(toUnits)"
-        } else {
-            print("Show alert here.")
-        }
+        let formatString = (decimalSegment.selectedSegmentIndex < decimalSegment.numberOfSegments-1 ? "%.\(decimalSegment.selectedSegmentIndex+1)f" : "%f")
+        let outputString = String(format: formatString, outputValue)
+        resultsOutlet.text = "\(inputValue) \(fromUnits) = \(outputString) \(toUnits)"
+        
     }
 
-    @IBAction func convertButtonPressed(_ sender: Any) {
+    @IBAction func decimalSelected(_ sender: Any) {
+        calculateConversion()
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        calculateConversion()
     }
     
 }
